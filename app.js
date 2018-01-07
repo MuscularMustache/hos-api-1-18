f38caddbc583192e08f08882c798028647e6082a
Nick Mele <nickmele26@gmail.com>
2018-01-07 16:25:38 -0500

    Add static schema file and yarn.lock

--- app.js
+++ app.js
@@ -10,4 +10,7 @@ const app = express();
 // bodyParser is needed just for POST.
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: myGraphQLSchema }));
 
+// GraphiQL, a visual editor for queries
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
 app.listen(PORT);
